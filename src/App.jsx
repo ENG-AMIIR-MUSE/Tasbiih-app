b75b1dc5d65228d1dcc4c38cf4dd20d1ffc414d5
Amiir <amirlearn08@gmail.com>
2024-03-03 01:20:25 -0800

    d

--- src/App.jsx
+++ src/App.jsx
@@ -3,14 +3,18 @@ import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
 import Tasbih from './Components/Tasbih'
+import Header from './Components/Header'
 
 function App() {
   const [count, setCount] = useState(0)
 
   return (
-
+    <>
+<Header/>
 <Tasbih/>
+
       
+    </>
 
   )
 }
---
+++ src/Components/Header.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+
+export default function Header() {
+  return (
+    <div>
+
+        <div>Logo</div>
+        <ul>
+            <li>HOme</li>
+            <li>HOme</li>
+            <li>HOme</li>
+            <li>HOme</li>
+        </ul>
+    </div>
+  )
+}
